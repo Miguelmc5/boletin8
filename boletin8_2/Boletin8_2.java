d578453db94a130c36b0ab716455e07814c76784
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-11-12 11:12:09 +0100

    clase principal

---
+++ boletin8_2/Boletin8_2.java
@@ -0,0 +1,24 @@
+package boletin8_2;
+
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class Boletin8_2 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Compra ticket=new Compra();
+    ticket.Descuentos();
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
