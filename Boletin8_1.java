7f99207a8f0d5487baa42f55cc9a4d80c33c2bdd
Miguel Martinez Costa <56074037+Miguelmc5@users.noreply.github.com>
2019-11-14 18:04:40 +0100

    Add files via upload

---
+++ Boletin8_1.java
@@ -0,0 +1,18 @@
+package boletin8_1;
+
+/**
+ *
+ * @author usuario
+ */
+public class Boletin8_1 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+     Porcentaje obx=new Porcentaje();
+        obx.setCantidad(obx.teclado);
+        obx.porcentajes();
+    }
+    
+}
